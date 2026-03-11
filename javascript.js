10f8334015e30e3e88db288d9a90c1e0992a48c2
Diego <108770121+Diego-Aco@users.noreply.github.com>
2026-03-11 17:03:29 -0500

    Adds functions for human and CPU choices

--- javascript.js
+++ javascript.js
@@ -1,1 +1,54 @@
-console.log("hello world!");
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+function numToChoice(num) {
+    switch(num) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    }
+}
+
+function getComputerChoice() {
+    const min = 1;
+    const max = 3;
+
+    const num = getRandomIntInclusive(min, max);
+    
+    return numToChoice(num);
+    // switch(num) {
+    //     case 1:
+    //         return "rock";
+    //         break;
+    //     case 2:
+    //         return "paper";
+    //         break;
+    //     case 3:
+    //         return "scissors";
+    //         break;
+    // }
+}
+
+function getHumanChoice() {
+    const num = Number(prompt("Enter a number for rock [1], paper [2], or scissors [3]"));
+    return numToChoice(num);
+}
+
+
+function nextRound() {
+    const humanChoice = getComputerChoice();
+    const cpuChoice = getComputerChoice();
+}
+
+const myChoice = getHumanChoice();
+console.log("You chose: " + myChoice);
+
